b6e041be6673b95a96e5f360306799b07c816d7d
Sunpil Kim <ezweb28@gmail.com>
2022-11-29 21:58:48 -0500

    Added HTTP POST UPDATE DELETE customer with Spring MVC

--- src/main/java/skim/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/skim/springframework/msscbrewery/web/controller/CustomerController.java
@@ -1,11 +1,9 @@
 package skim.springframework.msscbrewery.web.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import skim.springframework.msscbrewery.services.CustomerService;
 import skim.springframework.msscbrewery.web.model.CustomerDto;
 
@@ -25,5 +23,27 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId) {
 
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpHeaders> handlePost(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PostMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping({"/{customerId"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteById(customerId);
     }
 }
